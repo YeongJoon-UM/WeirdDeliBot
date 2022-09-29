01262637e0691502ddeb3425f1990f53241db486
JeongYeongJoon <71048593+JeongYeongJoon@users.noreply.github.com>
2022-09-29 14:21:13 +0900

    Initial Commit

---
+++ DazzleProject/DazzleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DazzleProjectApp.swift
+//  DazzleProject
+//
+//  Created by 정영준 on 2022/09/29.
+//
+
+import SwiftUI
+
+@main
+struct DazzleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
