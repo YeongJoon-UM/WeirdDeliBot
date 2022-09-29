54afdd58f3292c8f2dabb73d3aba6d0df4e6a316
JeongYeongJoon <71048593+JeongYeongJoon@users.noreply.github.com>
2022-09-29 14:32:44 +0900

    build_RootScreen_struct
    build RootScreen struct

--- DazzleProject/Screens/Root/RootScreen.swift
+++ DazzleProject/Screens/Root/RootScreen.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct RootScreen: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -19,8 +19,8 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct RootScreen_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        RootScreen()
     }
 }
